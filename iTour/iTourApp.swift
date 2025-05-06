b742f1d14c6407be469370797e8e5f8d401c3079
Ronak99 <punase.ronak99@gmail.com>
2025-05-06 10:16:25 +0530

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Ronak Pustack on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
